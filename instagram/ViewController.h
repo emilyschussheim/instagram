9d648f06802c5c311a4325378d3eee6c53bdf09c
Emily Schussheim <eschuss@gmail.com>
2018-07-08 18:59:55 -0700

    Initial Commit

---
+++ instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instagram
+//
+//  Created by Emily Schussheim on 7/8/18.
+//  Copyright © 2018 Emily Schussheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  instagram
+//
+//  Created by Emily Schussheim on 7/8/18.
+//  Copyright © 2018 Emily Schussheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
